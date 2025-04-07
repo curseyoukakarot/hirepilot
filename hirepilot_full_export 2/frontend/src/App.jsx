7a85a993d5de88674f9cc6f9122d3a723691e111
curseyoukakarot <brandon@offrgroup.com>
2025-04-06 19:10:20 -0500

    Update App.jsx

--- hirepilot_full_export 2/frontend/src/App.jsx
+++ hirepilot_full_export 2/frontend/src/App.jsx
@@ -1,22 +1,22 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SigninScreen from './screens/SigninScreen';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import SignupScreen from './screens/SignupScreen';
+import SigninScreen from './screens/SigninScreen';
 import OnboardingWizard from './screens/OnboardingWizard';
 import Dashboard from './screens/Dashboard';
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/signin" element={<SigninScreen />} />
-        <Route path="/signup" element={<SignupScreen />} />
-        <Route path="/onboarding" element={<OnboardingWizard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* Add more routes here as we expand */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/signin" />} />
+      <Route path="/signup" element={<SignupScreen />} />
+      <Route path="/signin" element={<SigninScreen />} />
+      <Route path="/onboarding" element={<OnboardingWizard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      {/* Add more routes as needed */}
+    </Routes>
   );
 }
+
+export default App;
